be1531badb4dc9e13e6a041ae86a29af0c22190a
LIVI5 <oooooes@g.skku.edu>
2025-03-14 11:10:11 +0900

    changing color of icons when it's active

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // html노드 중에 해당 변수에 할당시킴
-const icon = document.querySelector('.icon');
+const icons = document.querySelectorAll('.icon');
 const sub_btn = document.querySelector('.sub_btn');
 const down_btn = document.querySelector('#down_btn');
 const video_title = document.querySelector('#video_title');
 
 // 이벤트를 처리
-icon.addEventListener('click', () => {
-  icon.classList.toggle('active'); // active가 없다면 active/ active가 있다면 끔
+icons.forEach((icon) => {
+  icon.addEventListener('click', () => {
+    icon.classList.toggle('active');
+  });
 });
-
 sub_btn.addEventListener('click', () => {
   sub_btn.classList.toggle('active'); // active가 없다면 active/ active가 있다면 끔
 });
